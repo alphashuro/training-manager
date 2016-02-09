68695fac54f1557a7ea8ea1234409d08b112e512
Alpha Shuro <alphashuro@gmail.com>
2016-02-09 17:50:51 +0200

    import facilitators module

--- client/main.js
+++ client/main.js
@@ -4,7 +4,7 @@ import initContext from './configs/context';
 // modules
 import coreModule from './modules/core';
 import clientsModule from './modules/clients';
-// import facilitatorsModule from './modules/facilitators';
+import facilitatorsModule from './modules/facilitators';
 // import coursesModule from './modules/courses';
 // import bookingsModule from './modules/bookings';
 
@@ -15,7 +15,7 @@ const context = initContext();
 const app = createApp(context);
 app.loadModule(coreModule);
 app.loadModule(clientsModule);
-// app.loadModule(facilitatorsModule);
+app.loadModule(facilitatorsModule);
 // app.loadModule(coursesModule);
 // app.loadModule(bookingsModule);
 
