23ce074e68d76527ee98a6469ea824e9e85769af
Alpha Shuro <alphashuro@gmail.com>
2016-02-09 10:19:20 +0200

    use bootstrap for nav

--- client/modules/core/components/navigation.jsx
+++ client/modules/core/components/navigation.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-const Navigation = () => (
-  <div>
-    <b> Navigation: </b>
-    <a href="/"> Dashboard </a>
-    <a href="/clients">Clients</a>
-    <a href="/courses">Courses</a>
-    <a href="/facilitators">Facilitators</a>
-    <a href="/bookings">Bookings</a>
-  </div>
+const Navigation = ({onLogout, user}) => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        {user.profile.org}
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Nav bsStyle='tabs' activeKey={1}>
+      <NavItem eventKey={1} href="/"> Dashboard </NavItem>
+      <NavItem eventKey={2} href="/clients">Clients</NavItem>
+      <NavItem eventKey={3} href="/courses">Courses</NavItem>
+      <NavItem eventKey={4} href="/facilitators">Facilitators</NavItem>
+      <NavItem eventKey={5} href="/bookings">Bookings</NavItem>
+      <NavItem onClick={onLogout}>Logout</NavItem>
+    </Nav>
+  </Navbar>
 );
 
 export default Navigation;
