0048203aabaac2082b52875ff6852bf4098e4ba8
Alpha Shuro <alphashuro@gmail.com>
2016-02-16 11:06:13 +0200

    remove caching

--- client/modules/facilitators/containers/facilitator.js
+++ client/modules/facilitators/containers/facilitator.js
@@ -2,24 +2,13 @@ import Facilitator from '../components/facilitator.jsx';
 import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
 
 export const composer = ({context, facilitatorId, clearErrors}, onData) => {
-  const {Meteor, Collections, Tracker, LocalState} = context();
+  const {Meteor, Collections, LocalState} = context();
 
   Meteor.subscribe('facilitators.single', facilitatorId, () => {
     const facilitator = Collections.Facilitators.findOne(facilitatorId);
     const error = LocalState.get('FACILITATOR_ERROR');
     onData(null, {facilitator, error});
   });
-
-  // support latency compensation
-  const facilitatorFromCache = Tracker.nonreactive(() => {
-    return Collections.Facilitators.findOne(facilitatorId);
-  });
-
-  if (facilitatorFromCache) {
-    onData(null, {facilitator: facilitatorFromCache});
-  } else {
-    onData();
-  }
 
   return clearErrors;
 };
