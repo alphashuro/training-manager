d79e9246643e18efcafa666bcd2876db81eed51e
Alpha Shuro <alphashuro@gmail.com>
2016-02-16 16:00:52 +0200

    remove initial adds and downgrade jsdom

--- server/main.js
+++ server/main.js
@@ -1,9 +1,9 @@
 import publications from './publications';
 import methods from './methods';
-import addInitialData from './configs/initial_adds.js';
+// import addInitialData from './configs/initial_adds.js';
 import accountsConfig from './configs/accounts';
 
 publications();
 methods();
-addInitialData();
+// addInitialData();1
 accountsConfig();
