cb8ffbed1bf0400ab9182ba3b636ce16be237a0f
Alpha Shuro <alphashuro@gmail.com>
2016-02-17 11:09:22 +0200

    added setup for jsdom

--- .wallaby.js
+++ .wallaby.js
@@ -1,4 +1,6 @@
 module.exports = function (wallaby) {
+  var load = require;
+
   return {
     files: [
       'client/modules/**/components/*.jsx',
@@ -11,11 +13,33 @@ module.exports = function (wallaby) {
     ],
     compilers: {
       '**/*.js*': wallaby.compilers.babel({
-        babel: require('babel-core'),
+        babel: load('babel-core'),
         presets: [ 'es2015', 'stage-2', 'react' ]
       })
     },
     testFramework: 'mocha',
+    env: {
+      type: 'node'
+    },
+    setup: function( wallaby ) {
+      var jsdom = require('jsdom').jsdom;
+
+      var exposedProperties = [
+        'window', 'navigator', 'document'
+      ];
+
+      global.document = jsdom('');
+      global.window = document.defaultView;
+      Object.keys(document.defaultView).forEach((property) => {
+        if (typeof global[property] === 'undefined') {
+          exposedProperties.push(property);
+          global[property] = document.defaultView[property];
+        }
+      });
+      global.navigator = {
+        userAgent: 'node.js'
+      };
+    }
   };
 };
 
