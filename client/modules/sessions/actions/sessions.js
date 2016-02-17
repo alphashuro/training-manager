b91476e98332e4aee3390dd14a980354abaf5faa
Alpha Shuro <alphashuro@gmail.com>
2016-02-17 15:38:30 +0200

    reject if params missing

--- client/modules/sessions/actions/sessions.js
+++ client/modules/sessions/actions/sessions.js
@@ -1,5 +1,13 @@
 export default {
   update({ Meteor, LocalState }, id, { date }) {
+    if (!id) {
+      return LocalState.set('SESSIONS_ERROR', 'Id is required!');
+    }
+
+    if (!date) {
+      return LocalState.set('SESSIONS_ERROR', 'Date is required!');
+    }
+
     Meteor.call( 'sessions.update', id, { date }, (err) => {
       if (err) {
         LocalState.set('SESSIONS_ERROR', err.reason);
