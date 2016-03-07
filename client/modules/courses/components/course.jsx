ea1eecd6654e5518565bc0f511b5966382c509b6
Alpha Shuro <alphashuro@gmail.com>
2016-03-07 09:07:39 +0200

    refactor and add proptypes

--- client/modules/courses/components/course.jsx
+++ client/modules/courses/components/course.jsx
@@ -1,73 +1,75 @@
-
-import React, { Component } from 'react';
-
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { Panel } from 'react-bootstrap';
-import { Input } from 'react-bootstrap';
-import { PageHeader } from 'react-bootstrap';
-import { Alert } from 'react-bootstrap';
+import React, {PropTypes} from 'react';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import {Panel} from 'react-bootstrap';
+import {Input} from 'react-bootstrap';
+import {PageHeader} from 'react-bootstrap';
+import {Alert} from 'react-bootstrap';
 
 import ClassesList from '../../classes/containers/classes_list';
 
-class Course extends Component {
-  render() {
-    const {error, course} = this.props;
-    const { _id, title, description } = course;
-    return (
-      <div>
-        <PageHeader>
-          <span>{ title }</span>
-        </PageHeader>
+const Course = React.createClass({
+	propTypes: {
+		error: PropTypes.string,
+		course: PropTypes.shape({
+			_id: PropTypes.string,
+			title: PropTypes.string,
+			description: PropTypes.string
+		})
+	},
 
-        <Row>
-          <Col md={ 6 }>
-          <Panel>
-              <PageHeader>
-                  <span>Course Info</span>
-              </PageHeader>
-              { error ? <Alert bsStyle='danger'>{error}</Alert> : null }
-              <Input type="text"
-                 hasFeedback={ false }
-                 placeholder="Course title"
-                 label="Title"
-                 ref="titleRef"
-                 defaultValue={title}
-                ></Input>
-              <Input type="text"
-                 hasFeedback={ false }
-                 placeholder="Course description"
-                 label="Description"
-                 ref="descriptionRef"
-                 defaultValue={description}
-                ></Input>
-              <Button
-              className='save'
-              onClick={ this.saveCourse.bind(this) }
-              bsStyle="default">
-                  <span>Save</span>
-              </Button>
-          </Panel>
-          </Col>
-          <Col md={ 6 }>
-            <ClassesList courseId={ _id } />
-          </Col>
-        </Row>
-    </div>
-    );
-  }
+	render() {
+		const {error, course} = this.props;
+		const {_id, title, description} = course;
+		return (
+			<div>
+				<PageHeader>
+					<span>{title}</span>
+				</PageHeader>
 
-  saveCourse() {
-    const {update, course} = this.props;
-    const {titleRef, descriptionRef} = this.refs;
-
-    const title = titleRef.getValue();
-    const description = descriptionRef.getValue();
-
-    update(course._id, { title, description });
-  }
-}
+				<Row>
+					<Col md={6}>
+					<Panel>
+						<PageHeader>
+								<span>Course Info</span>
+						</PageHeader>
+						{error ? <Alert bsStyle='danger'>{error}</Alert> : null}
+						<Input type="text"
+							hasFeedback={false}
+							placeholder="Course title"
+							label="Title"
+							ref={node => this.title = node}
+							defaultValue={title}
+							></Input>
+						<Input type="text"
+							hasFeedback={false}
+							placeholder="Course description"
+							label="Description"
+							ref={node => this.description = node}
+							defaultValue={description}
+							></Input>
+						<Button
+							className='save'
+							onClick={() => {
+								const { update, course } = this.props;
+								update(course._id, {
+									title: this.title.getValue(),
+									description: this.description.getValue()
+								});
+							}}
+							bsStyle="default">
+								<span>Save</span>
+						</Button>
+					</Panel>
+					</Col>
+					<Col md={6}>
+						<ClassesList courseId={_id}/>
+					</Col>
+				</Row>
+		</div>
+		);
+	}
+});
 
 export default Course;
-
