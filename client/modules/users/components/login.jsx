ce1d71f65f7a5e07f1978ec65e9f928e5f195b67
Alpha Shuro <alphashuro@gmail.com>
2016-04-04 08:26:09 +0200

    refactor the login component

--- client/modules/users/components/login.jsx
+++ client/modules/users/components/login.jsx
@@ -1,55 +1,28 @@
 import React, {Component, PropTypes} from 'react';
 import { Grid, Row, Col, Well, Button, PageHeader, Input, Alert } from 'react-bootstrap';
 
-class Login extends Component {
-  render() {
-    const {error, handleLogin} = this.props;
-
-    return (
-      <Grid className='login'>
-        <Row>
-          <Col md={6} mdOffset={3}>
-            <Well>
-              <PageHeader>Login</PageHeader>
-              {
-                error ? <Alert bsStyle='danger'>{error}</Alert> : null
-              }
-              <Input
-                name='email'
-                id='email'
-                type='email'
-                ref={node => this.email = node}
-                placeholder='Your email address.'/>
-              <Input
-                name='password'
-                id='password'
-                type='password'
-                ref={node => this.password = node}
-                placeholder='Your password.'/>
-              <Button
-                className='login'
-                bsStyle='default'
-                onClick={() => {
-                  handleLogin(
-                    this.email.getValue(),
-                    this.password.getValue()
-                  )
-                }}>
-                  Login
-                </Button>
-              <Button
-                className='signup'
-                bsStyle='default'
-                href='/signup'>
-                  Sign up
-              </Button>
-            </Well>
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
-}
+const Login = ({error, handleLogin}) => (
+  <Grid className='login'>
+    <Row>
+      <Col md={6} mdOffset={3}>
+        <Well>
+          <PageHeader>Login</PageHeader>
+          { error ? <Alert bsStyle='danger'>{error}</Alert> : null }
+          <form className='login' onSubmit={handleLogin}>
+            <Input name='email' type='email' placeholder='Your email address.'/>
+            <Input name='password' type='password' placeholder='Your password.'/>
+            <Button bsStyle='default' type='submit'>
+              Login
+            </Button>
+          </form>
+          <Button className='signup' bsStyle='default' href='/signup'>
+              Sign up
+          </Button>
+        </Well>
+      </Col>
+    </Row>
+  </Grid>
+);
 
 Login.propTypes = {
   error: PropTypes.string,
--- client/modules/users/components/tests/login.js
+++ client/modules/users/components/tests/login.js
@@ -1,14 +1,14 @@
 const {describe, it} = global;
 import {expect} from 'chai';
 import {spy, stub, assert} from 'sinon';
-import {shallow, mount, render} from 'enzyme';
+import {shallow, render} from 'enzyme';
 import Login from '../login.jsx';
 import React from 'react';
 
 describe('users.components.login', () => {
   const getProps = () => ({
     error: null,
-    handleLogin: spy()
+    handleLogin: spy(),
   });
   it('should show error if there is error', () => {
     const props = getProps();
@@ -18,40 +18,31 @@ describe('users.components.login', () => {
     expect(el.text()).to.contain(props.error);
   });
 
-  it('should have an email input', () => {
+  it('form should have an email input', () => {
     const props = getProps();
     const el = render(<Login {...props}/>);
+    const form = el.find('form.login');
 
-    const emailInput = el.find(`input[id='email']`);
+    const emailInput = form.find(`input[name=email]`);
 
     expect(emailInput).to.have.length(1);
   });
-  it('should have a password input', () => {
+  it('form should have a password input', () => {
     const props = getProps();
     const el = render(<Login {...props}/>);
+    const form = el.find('form.login');
 
-    const passwordInput = el.find('input[id=password]');
+    const passwordInput = form.find('input[name=password]');
 
     expect(passwordInput).to.have.length(1);
   });
-  it('should call login with email and password when .login is clicked', () => {
+  it('should call handleLogin when form is submitted', () => {
     const props = getProps();
 
-    const el = mount(<Login {...props} />);
+    const el = shallow(<Login {...props} />);
 
-    el.find('input[id=email]').get(0).value = `email`;
-    el.find('input[id=password]').get(0).value = `password`;
+    el.find('form.login').simulate('submit');
 
-    el.find('button.login').simulate('click');
-
-    expect(props.login.calledOnce).to.be.equal(true);
-
-    const args = props.login.args[0];
-
-    expect(emailRef.getValue.calledOnce).to.be.equal(true);
-    expect(passwordRef.getValue.calledOnce).to.be.equal(true);
-
-    expect(args[0]).to.be.equal('email@address.com');
-    expect(args[1]).to.be.equal('password');
+    expect(props.handleLogin.calledOnce).to.be.equal(true);
   });
 });
