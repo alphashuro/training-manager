87ad2258cd2ff8124d70c420205da12adc83c1bb
Alpha Shuro <alphashuro@gmail.com>
2016-04-10 19:14:15 +0200

    "remove dependency on jsdom"

--- .wallaby.js
+++ .wallaby.js
@@ -20,26 +20,6 @@ module.exports = function (wallaby) {
     testFramework: 'mocha',
     env: {
       type: 'node'
-    },
-    setup: function( wallaby ) {
-      var jsdom = require('jsdom').jsdom;
-
-      var exposedProperties = [
-        'window', 'navigator', 'document'
-      ];
-
-      global.document = jsdom('');
-      global.window = document.defaultView;
-      Object.keys(document.defaultView).forEach((property) => {
-        if (typeof global[property] === 'undefined') {
-          exposedProperties.push(property);
-          global[property] = document.defaultView[property];
-        }
-      });
-      global.navigator = {
-        userAgent: 'node.js'
-      };
     }
   };
 };
-
