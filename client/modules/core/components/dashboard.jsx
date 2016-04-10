aa80d9c3b8d1576e987c226c154e3c388682c808
Alpha Shuro <alphashuro@gmail.com>
2016-04-10 23:18:47 +0200

    display messages when no data has been recieved

--- client/modules/core/components/dashboard.jsx
+++ client/modules/core/components/dashboard.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import EmptyRow from '../../util/components/EmptyRow.jsx';
+import { Panel, PageHeader, Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
 
-import { Panel, PageHeader, Row, Col } from 'react-bootstrap';
-
-const Dashboard = ({}) => (
+const Dashboard = ({ sessionsComingUp, statsThisMonth }) => (
   <Panel>
-    <PageHeader>
-      Dashboard
-    </PageHeader>
     <Row>
       <Col md={6}>
         <PageHeader>Coming up</PageHeader>
+        <ListGroup>
+        {
+          sessionsComingUp.length ? null : 'No sessions coming up this month.'
+        }
+        </ListGroup>
       </Col>
       <Col md={6}>
         <PageHeader>Stats this month</PageHeader>
+        <ListGroup>
+          {
+            statsThisMonth.length ? null : 'No data this month.'
+          }
+        </ListGroup>
       </Col>
     </Row>
   </Panel>
 );
+
+Dashboard.propTypes = {
+  sessionsComingUp: PropTypes.array.isRequired,
+  statsThisMonth: PropTypes.array.isRequired
+}
 
 export default Dashboard;
