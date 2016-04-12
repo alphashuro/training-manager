df977778e4632903f9c009291565b4d6bd4d755b
Alpha Shuro <alphashuro@gmail.com>
2016-04-12 15:15:23 +0200

    add basic enroll component

--- client/modules/users/components/enroll.jsx
+++ client/modules/users/components/enroll.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-const Enroll = ({token}) => (
-  <div>Enroll {token}</div>
+const Enroll = ({username, token, handleSubmit}) => (
+  <div>
+    <h2>Set your new password.</h2>
+    <p>{username}, Please set your new password.</p>
+    <form name="enroll" onSubmit={handleSubmit.bind(null, token)}>
+      <input name="password" type="password" placeholder="new password"/>
+      <input name="confirm" type="password" placeholder="confirm password"/>
+      <button type="submit">Save</button>
+    </form>
+  </div>
 );
+
+Enroll.propTypes = {
+  error: PropTypes.string,
+  username: PropTypes.string,
+  token: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+};
 
 export default Enroll;
