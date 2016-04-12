c917de16952a16fe50deb05075bcc19f38dc67ac
Alpha Shuro <alphashuro@gmail.com>
2016-04-12 15:15:37 +0200

    add invited user publication

--- server/publications/users.js
+++ server/publications/users.js
@@ -2,7 +2,10 @@ import {Users} from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
 
 export default function () {
-  Meteor.publish('users.enrolled', function (token) {
+  Meteor.publish('users.invited', function (token) {
+    check(token, String);
+    if (!this.userId) { return this.ready(); }
+
     return Users.find({'services.password.reset.token': token});
   });
 }
