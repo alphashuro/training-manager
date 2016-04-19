f69994d06c37efe7bf8f1995522e5bb818952eb7
Alpha Shuro <alphashuro@gmail.com>
2016-04-19 14:31:35 +0200

    add users publication

--- server/publications/users.js
+++ server/publications/users.js
@@ -8,4 +8,16 @@ export default function () {
 
     return Users.find({'services.password.reset.token': token});
   });
+  Meteor.publish('users.ids', function userIdsPublication() {
+    if (!this.userId) { return this.ready(); }
+    // FIXME: We'll need to check if this user is admin, but for now i'll just publish to all users
+
+    return Users.find();
+  });
+  Meteor.publish('users.single', function singleUserPublication(userId) {
+    check(userId, String);
+    if (!this.userId) { return this.ready(); }
+    // FIXME: Also need to check admin role here
+    return Users.find({_id: userId});
+  })
 }
