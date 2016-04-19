9a66d4a23c8780e71acde05184ac120ace0843dd
Alpha Shuro <alphashuro@gmail.com>
2016-04-19 16:16:04 +0200

    add method for updating users

--- server/methods/users.js
+++ server/methods/users.js
@@ -2,6 +2,7 @@ import {Meteor} from 'meteor/meteor';
 import {Accounts} from 'meteor/accounts-base';
 import {check} from 'meteor/check';
 import {Roles} from 'meteor/alanning:roles';
+import {Users} from '/lib/collections';
 
 export default function () {
   Meteor.methods({
@@ -28,6 +29,36 @@ export default function () {
       const id = Accounts.createUser(user);
       Roles.addUsersToRoles(id, 'admin');
       return id;
+    },
+    'users.update'(id, {name, phone, roles}) {
+      check(id, String);
+      check(name, String);
+      check(roles, Array);
+
+      if (!id) {
+        throw new Meteor.Error('id-not-given', 'An id is required to update a user');
+      }
+
+      if (!name) {
+        throw new Meteor.Error('name-not-given', 'A name is required to update a user');
+      }
+
+      if (!phone) {
+        throw new Meteor.Error('phone-not-given', 'A phone is required to update a user');
+      }
+
+      if (!roles) {
+        throw new Meteor.Error('roles-not-given', 'Roles are required to update a user');
+      }
+
+      Users.update(id, {
+        $set: {
+          'profile.name': name,
+          'profile.phone': phone
+        }
+      });
+
+      Roles.setUserRoles(id, roles);
     }
   });
 }
